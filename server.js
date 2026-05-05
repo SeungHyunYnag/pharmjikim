7a8070078c6397f28827293729c7e26f1a264823
SeungHyunYnag <sambapati@nate.com>
2026-05-06 00:30:50 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
@@ -53,6 +54,6 @@ app.get("/api/pharmacies", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
